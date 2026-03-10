55eff2d3c03a3d242c0d662cb1758b481b3f088c
deselec-catalog <deselecyeray@gmail.com>
2026-03-10 10:15:45 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,6 @@
 // ===== CONFIGURACIÓN DE FIREBASE =====
+// Import the functions you need from the SDKs you need
+// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCieW2pgIbGFqwBPkTQ6IK4a6QDbdoMvog",
     authDomain: "stockmaster-173bd.firebaseapp.com",
@@ -7,6 +9,9 @@ const firebaseConfig = {
     messagingSenderId: "1016129124873",
     appId: "1:1016129124873:web:316bf3b649248d0b6696d8"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
